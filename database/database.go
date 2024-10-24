828092037004b365bfd6a218a3eda6a36e3e025f
Luka Sijic <23730024+luka-sijic@users.noreply.github.com>
2024-10-24 02:31:11 +0200

    Create database.go

---
+++ database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+        "fmt"
+        "context"
+        "github.com/joho/godotenv"
+        "github.com/jackc/pgx/v4/pgxpool"
+        "log"
+        "os"
+)
+
+var DB *pgxpool.Pool
+
+func init() {
+    // Load the .env file
+    err := godotenv.Load("./.env")
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+}
+
+func Connect() {
+        dbURL := os.Getenv("DATABASE_URL")
+        if dbURL == "" {
+        log.Fatal("DATABASE_URL is not set")
+    }
+        var err error
+        DB, err = pgxpool.Connect(context.Background(), dbURL)
+        if err != nil {
+                log.Fatalf("Unable to connect to database: %v\n", err)
+        }
+        fmt.Println("Database connected")
+}
+
+func Close() {
+        DB.Close()
+}
